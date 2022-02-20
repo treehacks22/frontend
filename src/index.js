52c785e02362299d4b9eda3bf58b8a5d1fe971a9
Maria Sitkovets <m.sitkovets@gmail.com>
2022-02-20 01:15:49 -0500

    Add ReactRTC to index

--- src/index.js
+++ src/index.js
@@ -7,12 +7,17 @@ import Jam from './pages/Jam'
 import JamMenu from './pages/JamMenu'
 import './index.css'
 
+import RTCMesh from './components/ReactRTC/RTCMesh.js';
+import RTCMain from './components/RTCMain.js';
+
+const WEBSOCKET_URL = 'wss://192.168.0.17:4000/ws';
+
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<App />}></Route>
-      <Route path='band' element={<Band />}></Route>
-      <Route path='jam' element={<Jam />}></Route>
+      <Route path='/' render={(props)=> <RTCMain URL={WEBSOCKET_URL} />} element={<App />}></Route>
+      <Route path='band' render={(props)=> <RTCMesh URL={WEBSOCKET_URL} />}  element={<Band />}></Route>
+      <Route path='jam' render={(props)=> <RTCMesh URL={WEBSOCKET_URL} />} element={<Jam />}></Route>
       <Route path='jammenu' element={<JamMenu />}></Route>
     </Routes>
   </BrowserRouter>,

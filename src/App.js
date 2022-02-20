7a9c10d1aadb54a21d979201468a7820f48da020
Shelby <shelby@9count.co>
2022-02-19 18:10:07 -0800

    home screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { Link } from 'react-router-dom'
 import './App.css'
+import background from './images/home_background.png'
+import Button from '@mui/material/Button';
+import Band from './Band';
 
 function App() {
   return (
-    <div>
+    <div style={{ height:"100vh", backgroundImage: `url(${background})`,  backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundColor: 'black', backgroundPosition: 'center'}}>
+      {"welcome"}
       <nav>
-        <Link to='/band'>Band</Link>
+        <Button  variant="outlined" size = 'large' component={Link} to="/jam" style={{maxWidth: '150px', maxHeight: '150px', minWidth: '150px', minHeight: '150px'}}>Jam</Button>
+
       </nav>
       <nav>
-        <Link to='/jam'>Jam</Link>
+        <Button variant="outlined" size = 'large' component={Link} to="/band">Band</Button>
       </nav>
     </div>
   )

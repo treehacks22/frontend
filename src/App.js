852f5f8ba235af72e120236cfb8be9dc0dc92eba
Maria Sitkovets <m.sitkovets@gmail.com>
2022-02-19 22:24:06 -0500

    Main menu styling

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import { Link } from 'react-router-dom'
 import './App.css'
 import background from './images/home_background.png'
 import Button from '@mui/material/Button';
-import Band from './Band';
 
 function App() {
   return (
     <div style={{ height:"100vh", backgroundImage: `url(${background})`,  backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundColor: 'black', backgroundPosition: 'center'}}>
       {"welcome"}
       <nav>
-        <Button  variant="outlined" size = 'large' component={Link} to="/jam" style={{maxWidth: '150px', maxHeight: '150px', minWidth: '150px', minHeight: '150px'}}>Jam</Button>
-
+        <Button size = 'large' component={Link} to="/jam" style={{maxWidth: '150px', maxHeight: '150px', minWidth: '150px', minHeight: '150px', margin: '50px', fontFamily: 'GuitarHero', color: '#00FFFF', fontSize: '40px'}}>Jam</Button>
       </nav>
       <nav>
-        <Button variant="outlined" size = 'large' component={Link} to="/band">Band</Button>
+        <Button size = 'large' component={Link} to="/band" style={{maxWidth: '150px', maxHeight: '150px', minWidth: '150px', minHeight: '150px', margin: '50px', float: 'right', fontFamily: 'GuitarHero', color: '#00FFFF', fontSize: '40px'}}>Band</Button>
       </nav>
     </div>
   )
